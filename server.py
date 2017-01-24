00e4def4cf83b05f43ef29932f164fe2af544e76
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2017-01-24 18:27:20 +0100

    Add asyncio python server

---
+++ server.py
@@ -0,0 +1,39 @@
+from typing import List
+import asyncio
+
+loop = asyncio.get_event_loop()
+
+clients = []  # type: List[EchoProtocol]
+
+
+class EchoProtocol(asyncio.Protocol):
+    def __init__(self):
+        self.transport = None  # type: asyncio.Transport
+
+    def connection_made(self, transport):
+        self.transport = transport
+        clients.append(self)
+        print("Client connected, %d clients" % len(clients))
+
+    def data_received(self, data):
+        for client in clients:
+            client.transport.write(data)
+
+    def connection_lost(self, exc):
+        clients.remove(self)
+        print("Client disconnected, %d clients" % len(clients))
+
+
+server = loop.run_until_complete(
+    loop.create_server(EchoProtocol, port=4242)
+)
+
+print("Started")
+try:
+    loop.run_forever()
+except:
+    pass
+
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
